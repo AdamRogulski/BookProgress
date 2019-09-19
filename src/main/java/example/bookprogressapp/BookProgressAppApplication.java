94f0b26724e5da2ee710ad426b5ffbcbf58de58f
Adam <adamwrogulski@gmail.com>
2019-09-19 15:22:59 +0200

    Initial commit

---
+++ src/main/java/example/bookprogressapp/BookProgressAppApplication.java
@@ -0,0 +1,13 @@
+package example.bookprogressapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookProgressAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookProgressAppApplication.class, args);
+    }
+
+}
